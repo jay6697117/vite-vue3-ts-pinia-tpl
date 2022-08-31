2097eb6e04f7ba0b80f6fb2bcd239699e14ebe31
steve zhang <871506263@qq.com>
2022-08-31 22:09:28 +0800

    init

--- src/pages/request.vue
+++ src/pages/request.vue
@@ -2,7 +2,7 @@
   <h2> 这里是request请求页面 </h2>
   <br />
   <router-link to="/">点击跳转至首页</router-link>
-  <button @click="requestRes()"></button>
+  <button @click="requestRes">requestRes</button>
 </template>
 <script lang="ts">
   import { defineComponent } from 'vue';
