ccc0b17e76498ccc6dc0b8d1b5e2d0d216397a86
steve zhang <871506263@qq.com>
2022-08-31 22:01:10 +0800

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  vueIndentScriptAndStyle: true,
+  singleQuote: true, // 单引号
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  insertPragma: false,
+  requirePragma: false,
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'auto'
+};
